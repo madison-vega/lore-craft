e4dc7d4a24bf320d00a635b7249a78eaa32e72d4
Angela Rubio <angelacr@ymail.com>
2021-05-01 11:39:12 -0500

    Acr (#5)
    * working on api index home and user
    
    * add email in user models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,8 @@ const { Character, Game, Faction, Race, LinkTag, User} = require('../models');
 
 router.get('/', (req, res) => {
     res.render('homepage')
-})
+});
+
+
 
 module.exports = homepage;
