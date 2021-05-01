d086a5d9d1a906ec0600e4d90e3c3736160b3302
Samuel Cordova <cordovas1337@gmail.com>
2021-05-01 12:05:54 -0500

    added some game seeds data

--- seeds/game-seeds.js
+++ seeds/game-seeds.js
@@ -1,17 +1,17 @@
 const { Game } = require('../models');
 
-// const gameData = [
-//   {
-//     game_name: 'Warcraft 3 Reign Of Chaos',
-//   },
-//   {
-//     game_name: 'Warcraft 3: The Frozen Throne',
-//   },
-//   {
-//     game_name: 'World Of Warcraft',
-//   },
+const gameData = [
+  {
+    game_name: 'Warcraft 3 Reign Of Chaos',
+  },
+  {
+    game_name: 'Warcraft 3: The Frozen Throne',
+  },
+  {
+    game_name: 'World Of Warcraft',
+  },
 
-// ];
+];
 
 const seedGames = () => Game.bulkCreate(gameData);
 
