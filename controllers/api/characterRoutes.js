cbf2382c1563c06f893edc59cece4976e7ddc047
Madison Vega <madison.r.vega@gmail.com>
2021-05-01 13:30:34 -0500

    added get routes for character

---
+++ controllers/api/characterRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { Character, Faction, Game, Race, LinkTag } = require('../../models')
+
+router.get('/', async (req, res) => {
+    try {
+        const character = await Character.findAll({
+            include: [Faction, Game, Race, LinkTag]
+        });
+        res.status(200).json(character)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+router.get('/:id', async (req, res) => {
+    try {
+        const oneChar = await Character.findByPk(req.params.id, {
+            include: [Faction, Game, Race, LinkTag]
+        })
+        res.status(200).json(oneChar)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
