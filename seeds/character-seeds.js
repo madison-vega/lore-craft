4a0bf6175d5d3251e0636b756bc5194376294a62
Samuel Cordova <cordovas1337@gmail.com>
2021-05-05 18:37:23 -0500

    changed character seed

--- seeds/character-seeds.js
+++ seeds/character-seeds.js
@@ -4,69 +4,69 @@ const CharacterData = [
   {
     character_name: 'Sylvanus Windrunner',
     age: 23,
-    first_appearance: 14,
-    faction_id: 1,
+    first_appearance: 3,
+    faction_id: 2,
     race_id: 1,
     bio: 'none',
   },
   {
-    character_name: 'Arthus Menethil',
+    character_name: 'Prince Arthus Menethil',
     age: 90,
-    first_appearance: 25,
-    faction_id: 5,
+    first_appearance: 3,
+    faction_id: 1,
     race_id: 1,
     bio: 'none'
   },
   {
     character_name: 'Muradin Bronzebeard',
     age: 22,
-    first_appearance: 12,
-    faction_id: 4,
-    race_id: 1,
+    first_appearance: 3,
+    faction_id: 1,
+    race_id: 3,
     bio: 'none'
   },
   {
     character_name: 'Illidian Stormrage',
     age: 24,
-    first_appearance: 50,
-    faction_id: 3,
-    race_id: 1,
+    first_appearance: 3,
+    faction_id: 1,
+    race_id: 5,
     bio: 'none'
   },
   {
     character_name: 'Thrall',
-    price: 29,
-    stock: 22,
-    category_id: 2,
-    race_id: 1,
+    age: 29,
+    first_appearance: 3,
+    faction_id: 2,
+    race_id: 13,
     bio: 'none'
   },
   {
     character_name: 'Jaina Proudmoore',
-    price: 29,
-    stock: 22,
-    category_id: 2,
+    age: 29,
+    first_appearance: 3,
+    faction_id: 1,
     race_id: 1,
     bio: 'none'
   },
   {
     character_name: 'Malfurion Stormragre',
-    price: 29,
-    stock: 22,
-    category_id: 2,
-    race_id: 1,
+    age: 29,
+    first_appearance: 3,
+    faction_id: 1,
+    race_id: 5,
     bio: 'none'
   },
   {
     character_name: 'Durotan',
-    price: 29,
-    stock: 22,
-    category_id: 2,
-    race_id: 1,
+    age: 29,
+    first_appearance: 5,
+    faction_id: 2,
+    race_id: 14,
     bio: 'none'
   },
 ];
 
-const seedCharacters = () => Product.bulkCreate(characterData);
+const seedCharacters = () => Character.bulkCreate(characterData);
 
 module.exports = seedCharacters;
