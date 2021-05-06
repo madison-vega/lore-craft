58da83071e127bc07962e1dfeaaac6ad404f836e
Madison Vega <madison.r.vega@gmail.com>
2021-05-05 21:19:11 -0500

    fixed issues with seeds, and model relationships

--- seeds/character-seeds.js
+++ seeds/character-seeds.js
@@ -1,68 +1,68 @@
 const { Character } = require('../models');
 
-const CharacterData = [
+const characterData = [
   {
     character_name: 'Sylvanus Windrunner',
     age: 23,
     first_appearance: 3,
-    faction_id: 2,
-    race_id: 1,
+    faction_char: 2,
+    race: 1,
     bio: 'none',
   },
   {
     character_name: 'Prince Arthus Menethil',
     age: 90,
     first_appearance: 3,
-    faction_id: 1,
-    race_id: 1,
+    faction_char: 1,
+    race: 1,
     bio: 'none'
   },
   {
     character_name: 'Muradin Bronzebeard',
     age: 22,
     first_appearance: 3,
-    faction_id: 1,
-    race_id: 3,
+    faction_char: 1,
+    race: 3,
     bio: 'none'
   },
   {
     character_name: 'Illidian Stormrage',
     age: 24,
     first_appearance: 3,
-    faction_id: 1,
-    race_id: 5,
+    faction_char: 1,
+    race: 5,
     bio: 'none'
   },
   {
     character_name: 'Thrall',
     age: 29,
     first_appearance: 3,
-    faction_id: 2,
-    race_id: 13,
+    faction_char: 2,
+    race: 13,
     bio: 'none'
   },
   {
     character_name: 'Jaina Proudmoore',
     age: 29,
     first_appearance: 3,
-    faction_id: 1,
-    race_id: 1,
+    faction_char: 1,
+    race: 1,
     bio: 'none'
   },
   {
     character_name: 'Malfurion Stormragre',
     age: 29,
     first_appearance: 3,
-    faction_id: 1,
-    race_id: 5,
+    faction_char: 1,
+    race: 5,
     bio: 'none'
   },
   {
     character_name: 'Durotan',
     age: 29,
     first_appearance: 5,
-    faction_id: 2,
-    race_id: 14,
+    faction_char: 2,
+    race: 14,
     bio: 'none'
   },
 ];
