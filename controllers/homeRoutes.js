40a253e2f6a5b14460b92defc50d2e780c70d20b
Core Alcoser <corrinaalcoser@gmail.com>
2021-05-05 20:54:58 -0500

    Raid (#29)
    * adding views for handlebars
    
    * edited handlebar pages to use bulma
    
    * added navbar to the main page
    
    * added navbar to the main page
    
    * added login and log out scripts
    
    * edited handlebars
    
    * added homepage info and flexbox layout
    
    * added homepage info and flexbox layout
    
    * added logout message
    
    * added logout modal
    
    * added router use to index.js
    
    * added route for homepage
    
    * edited view engine
    
    * edited header
    
    * edited header
    
    * added cors package
    
    * edited css links
    
    * added fave icon and navbar icon

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Character, Game, Faction, Race, LinkTag, User} = require('../models');
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
+app.use(cors);
+ 
+var corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-router.get('/', (req, res) => {
-    res.render('homepage')
+router.get('/', cors(corsOptions), (req, res) => {
+    res.render('homepage');
 });
 
 
