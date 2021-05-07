95f03909761e0dab9ef9acc36dc8fd1551fb6ad6
Madison <madison.r.vega@gmail.com>
2021-05-06 20:16:14 -0500

    Maddib (#35)
    * added auth helper
    
    * added get routes for character
    
    * added routes for character made some changes
    
    * added new character model
    
    * added new character relationship in models, added ids in link tag for user and new character, started newCharacterRoute
    
    * fixed typo
    
    * added new character routes
    
    * fixed server.js to include all middleware and dependencies needed
    
    * added info to readme
    
    * fixed issues with seeds, and model relationships
    
    * fixed route to find characters
    
    * refactored gameroutes to be try catch and async and await
    
    * refactored user routes to be try catch and async and await, added login render to homeroutes, removed gets from user routes added with auth to new character routes

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -5,7 +5,7 @@ const { Game, Character, Faction, Race, LinkTag } = require('../../models');
 router.get('/', async (req, res) => {
     console.log(req.body.game_name)
     try {
-        let allGames = await Game.findAll({
+        const allGames = await Game.findAll({
             include: Character,
             where: {
                 game_name: req.body.game_name
@@ -19,62 +19,23 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        let 
-
+        const oneGame = await Game.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: {
+                model: Faction, Race, LinkTag,
+                attributes: []
+            }
+        });
+        res.status(200).json(oneGame)
     } catch (err) {
-
-
-    }
-
-    Game.findOne({
-        where: {
-            id: req.params.id
-        },
-        include: {
-            model: Faction, Race, LinkTag,
-            attributes: []
-        }
-    })
-    .then(gameAdded => res.json(gameAdded))
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-
-router.post('/', async (req, res) => {
-  Game.create({
-      game_name: req.body.game_name
-  })
-    .then(gameAdded => res.json(gameAdded))
-    .catch(err => {
-        console.log(err);
         res.status(500).json(err);
-    });
-});
-
-
-router.put('/id', (req, res) => {
-    Game.update({
-        game_name: req.body.game_name
-    },
-    where, {
-        id: req.params.id
     }
-    )
-    .then(gameAdded => {
-        if (!gameAdded) {
-            res.status(404).json({ message: 'Sorry no game found with this id'});
-            return;
-        }
-        res.json(gameAdded);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
+
+
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,32 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Character, Game, Faction, Race, LinkTag, User} = require('../models');
+const { Character, Game, Faction, Race, LinkTag, User } = require('../models');
 var express = require('express');
 var cors = require('cors');
 var app = express();
 
 app.use(cors);
- 
+
 var corsOptions = {
   origin: 'http://localhost:3001',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 router.get('/', cors(corsOptions), (req, res) => {
-    res.render('homepage');
+  res.render('homepage');
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/newCharacter');
+    return;
+  } else {
+    res.render('login');
+  }
+
+});
+
+
 
 
 
