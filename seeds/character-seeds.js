8d1441f6dad3e9efdea928e660e0955253f49e75
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2021-05-08 10:29:56 -0500

    Horde (#36)
    * added code for the models as well as connections and server WIP might need to go back and do some editing
    
    * changed some comments
    
    * added some seed data for characters
    
    * added the faction code for seeds
    
    * added some game seeds data
    
    * added some more seed data and the race seeds
    
    * minor tweak need to work on link tag seeds next
    
    * added the link tags and changed some relationships
    
    * added sequelize
    
    * few changes
    
    * changed character seed
    
    * added character bios and some new chars
    
    * added some link tag
    
    * changed something in the character seeds
    
    * added bios and replaced durotan
    
    * added images and fixed character name
    
    * after lots of testing for some stuff to populate
    
    * finished editing bios to correct length

--- seeds/character-seeds.js
+++ seeds/character-seeds.js
@@ -23,7 +23,7 @@ const characterData = [
     first_appearance: 3,
     faction_char: 1,
     race: 3,
-    bio: 'none'
+    bio: 'Muradin Bronzebeard is the High Thane of the Bronzebeard clan and the younger brother of the dwarven King Magni Bronzebeard and the older brother of the renowned explorer Brann Bronzebeard. He is now at the throne of Ironforge'
   },
   {
     character_name: 'Illidian Stormrage',
@@ -31,7 +31,7 @@ const characterData = [
     first_appearance: 3,
     faction_char: 1,
     race: 5,
-    bio: 'none'
+    bio: 'Illidan Stormrage, commonly known as the Betrayer, is the first of the demon hunters, the former self-proclaimed Lord of Outland, the former ruler of the Black Temple, and the original leader of the Illidari. He is the twin brother of Malfurion Stormrage.'
   },
   {
     character_name: 'Thrall',
@@ -39,15 +39,15 @@ const characterData = [
     first_appearance: 3,
     faction_char: 2,
     race: 13,
-    bio: 'none'
+    bio: 'Born as Goel, the son of the Durotan, chieftan of the Frostwollf clan, after the second war, he was found next to his dead parents by the human Aedelas Blackmoore, who took him in as a gladiator and slave, then appropriately named; Thrall.'
   },
   {
     character_name: 'Jaina Proudmoore',
-    age: 37-40,
+    age: 38,
     first_appearance: 3,
     faction_char: 1,
     race: 1,
-    bio: 'none'
+    bio: 'Lady Jaina Proudmoore is the daughter of Daelin and Katherine Proudmoore, and sister of Derek and Tandred. She is considered one of the finest mages in all Azeroth and the most powerful human sorceress alive.'
   },
   {
     character_name: 'Malfurion Stormragre',
@@ -55,7 +55,7 @@ const characterData = [
     first_appearance: 3,
     faction_char: 1,
     race: 5,
-    bio: 'none'
+    bio: 'Malfurion Stormrage was the first mortal druid on Azeroth, the night elf who initiated the use of druidism among the kaldorei people ten millennia ago under tutelage of the demigod Cenarius. Twin brother of Illidan Stormrage.'
   },
   {
     character_name: 'Grom Hellscream',
@@ -63,7 +63,7 @@ const characterData = [
     first_appearance: 2,
     faction_char: 2,
     race: 13,
-    bio: 'none'
+    bio: 'Grommash "Grom" Hellscream was the legendary chieftain of the Warsong clan, a powerful warrior, close friend, and greatest lieutenant to Warchief Thrall. He bound the Horde to the Burning Legion by drinking blood of Mannoroth the Destructor,'
   },
 ];
 
