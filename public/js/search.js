9fd3461a1e757215a03212116b16f34fc0eb571b
Madison Vega <madison.r.vega@gmail.com>
2021-05-07 21:26:06 -0500

    added base search.js file to get data from database when user searches

---
+++ public/js/search.js
@@ -0,0 +1,25 @@
+const searchHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+
+    // Gather the data from the form elements on the page
+    const searchContent = document.querySelector('searchbox').value.trim();
+    
+
+    if (searchContent) {
+        const response = await fetch('/api/character', {
+            method: 'GET',
+            body: JSON.stringify({ response: searchContent }),
+            headers: { 'Content-Type': 'application/json' },
+            
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('No results matching search criteria.');
+        }
+    }
+};
+
+document.querySelector('searchbutton').addEventListener('submit', searchHandler);
